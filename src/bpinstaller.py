a4cfa69e9c08d24b190abe73c40228f69c77c2cd
Naeim Cragwell-Chaudhry <naeimc@gmail.com>
2019-04-14 22:28:53 +0100

    Update bpinstaller.py

--- src/bpinstaller.py
+++ src/bpinstaller.py
@@ -10,7 +10,6 @@ test_schematic = """
             "username" : "",
             "password: : "",
             "connection" : "",
-            "create" : "True"
         }
     ]
 }
@@ -26,25 +25,20 @@ class SchematicInterface:
 
 class BluePrismInterface:
 
-    bpInstallDir = "C:\\Program Files\\Blue Prism Limited\\Blue Prism Automate"
-    automate = bpInstallDir + "\\Automate.exe"
-    automateC = bpInstallDir + "\\AutomateC.exe"
+    bp_install_dir = "C:\\Program Files\\Blue Prism Limited\\Blue Prism Automate"
+
+    bp_server_connection_keys = []
 
     @classmethod
-    def set_current_connection(cls, connection_name):
-        a = [cls.automate, "/dbconname", connection_name]
-        print(a)
+    def _create_db_connection_sql_auth(cls, database_server_name, database_name, username="", password="", connection_name=""):
+        a = [cls.automate(), "/setdbserver", database_server_name, "/setdbname", database_name]
 
-    @classmethod
-    def create_db_connection(cls, database_server_name, database_name, username="", password="", connection_name="", create=False):
-        a = [cls.automate, "/setdbserver", database_server_name, "/setdbname", database_name]
-
-        if len(username) != 0 and len(password) != 0:
-            a += ["/setdbusername", username, "/setdbpassword", password]
-        elif len(username) != 0:
-            raise Exception("A Username was not supplied during Database Connection creation.")
+        if len(username) != 0:
+            raise Exception("A username was not supplied.")
         elif len(password) != 0:
-            raise Exception("A Password was not supplied during Database Connection creation.")
+            raise Exception("A password was not supplied.")
+        else:
+            a += ["/setdbusername", username, "/setdbpassword", password]
 
         if len(connection_name) > 0:
             a += ["/dbconname", connection_name]
@@ -52,17 +46,25 @@ class BluePrismInterface:
         subprocess.call(a)
 
     @classmethod
-    def create_bp_connection(cls, host, port, connection_name="", connection_mode=""):
-        a = [cls.automate, "/setbpserver", host, port]
+    def _create_db_connection_win_auth(cls, server_name, database_name, connection_name=""):
+        a = [cls.automate(), "/setdbserver", server_name, "/setdbname", database_name]
+
+        if len(connection_name) > 0:
+            a += ["/dbconname", connection_name]
+
+        subprocess.call(a)
+
+    @classmethod
+    def _create_bp_server_connection(cls, host, port, connection_name="", connection_mode=""):
+        a = [cls.automate(), "/setbpserver", host, port]
 
         if len(connection_name) > 0:
             a += ["/dbconname", connection_name]
 
         if len(connection_mode) > 0:
-            if connection_mode.isdigit():
-                if int(connection_mode) < 0 or int(connection_mode) > 5:
-                    raise Exception("The connection mode must be between 0 and 5.")
-            else:
+            if connection_mode.isdigit() and (int(connection_mode) < 0 or int(connection_mode) > 5):
+                raise Exception("The Connection Mode must be between 0 and 5.")
+            elif not connection_mode.isdigit():
                 raise Exception("The Connection Mode supplied must be an integer.")
 
             a += ["/connectionmode", connection_mode]
@@ -70,13 +72,26 @@ class BluePrismInterface:
         subprocess.call(a)
 
     @classmethod
-    def open(cls):
-        subprocess.call([cls.automate])
+    def setup(cls, install_dir):
+        cls.bp_install_dir = install_dir
 
     @classmethod
-    def setup(cls, bp_install_dir):
-        cls.bpInstallDir = bp_install_dir
-        cls.automate = bp_install_dir + "\\Automate.exe"
-        cls.automateC = bp_install_dir + "\\AutomateC.exe"
+    def automate(cls, run=False):
+        path = cls.bp_install_dir + "\\Automate.exe"
 
-BluePrismInterface().set_current_connection("test")
+        if run:
+            subprocess.call([path])
+
+        return path
+
+    @classmethod
+    def automate_c(cls, run=False):
+        path = cls.bp_install_dir + "\\AutomateC.exe"
+
+        if run:
+            subprocess.call([path])
+
+        return path
+
+
+BluePrismInterface.automate(True)
