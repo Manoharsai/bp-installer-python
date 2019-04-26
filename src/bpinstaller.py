ac4c0ee40404cf8e9aaf8db5fd58782b63f6999d
Naeim Cragwell-Chaudhry <naeimc@gmail.com>
2019-04-26 16:12:27 +0100

    Update bpinstaller.py

--- src/bpinstaller.py
+++ src/bpinstaller.py
@@ -1,9 +1,28 @@
 import subprocess
 
 
+class BluePrismInstaller:
+
+    bp_installer_location = ""
+
+    def __int__(self):
+        pass
+
+    @classmethod
+    def set_bp_installer_location(cls, installer_location):
+        cls.bp_installer_location = installer_location
+
+    @classmethod
+    def install_blue_prism(cls):
+        pass
+
+
 class BluePrismInterface:
 
     bp_install_dir = "C:\\Program Files\\Blue Prism Limited\\Blue Prism Automate"
+
+    def __init__(self):
+        pass
 
     @classmethod
     def create_db_server_connection(cls, database_server_name, database_name, username="", password="", connection_name=""):
@@ -17,7 +36,7 @@ class BluePrismInterface:
                 raise Exception("A username AND password must be supplied if an SQL database connection is to be used.")
             a += ["/setdbusername", username, "/setdbpassword", password]
 
-        subprocess.call(a)
+        return a
 
     @classmethod
     def create_db(cls, password="", connection_name=""):
@@ -26,7 +45,7 @@ class BluePrismInterface:
         if len(connection_name) > 0:
             a += ["/dbconname", connection_name]
 
-        subprocess.call(a)
+        return a
 
     @classmethod
     def create_bp_server_connection(cls, host, port, connection_name="", connection_mode=""):
@@ -43,29 +62,33 @@ class BluePrismInterface:
 
             a += ["/connectionmode", connection_mode]
 
-        subprocess.call(a)
+        return a
 
     @classmethod
     def set_bp_install_dir(cls, install_dir):
         cls.bp_install_dir = install_dir
 
     @classmethod
-    def automate(cls, run=False):
-        path = cls.bp_install_dir + "\\Automate.exe"
+    def automate(cls):
+        a = cls.bp_install_dir + "\\Automate.exe"
 
-        if run:
-            subprocess.call([path])
-
-        return path
+        return a
 
     @classmethod
-    def automate_c(cls, run=False):
-        path = cls.bp_install_dir + "\\AutomateC.exe"
+    def automate_c(cls):
+        a = cls.bp_install_dir + "\\AutomateC.exe"
 
-        if run:
-            subprocess.call([path])
-
-        return path
+        return a
 
 
-BluePrismInterface.automate(True)
+class CommandLineInterface:
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def run_command(cls, command):
+        subprocess.call(command)
+
+
+CommandLineInterface.run_command(BluePrismInterface.automate())
