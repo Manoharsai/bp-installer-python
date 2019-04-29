38ec6c91e0c9f4d491e02e923271c5aad1ac7d96
Naeim Cragwell-Chaudhry <naeimc@gmail.com>
2019-04-29 22:15:13 +0100

    Update bpinstaller.py

--- src/bpinstaller.py
+++ src/bpinstaller.py
@@ -1,52 +1,149 @@
-import subprocess
+import os
+import json
+
+
+class Schematic:
+
+    @classmethod
+    def get(cls, path):
+
+        f = open(path)
+        contents = f.read()
+        f.close()
+
+        if os.path.splitext(path) == ".json":
+            return json.loads(contents)
+        elif os.path.splitext(path) == ".xml":
+            return
+        raise Exception("Unknown file type.")
 
 
 class BluePrismCommands:
 
-    _directory = "C:\\Program Files\\"
-
-    _standard_bp_install_folder = "Blue Prism Limited\\Blue Prism Automate"
-
-    _automate_exe = "\\Automate.exe"
-    _automate_c_exe = "\\AutomateC.exe"
-
-    _automate = ""
-    _automate_c = ""
-
-    _arguments = [
+    _install_directory = "C:\\Program Files\\Blue Prism Limited\\Blue Prism Automate"
+    _delimiter = "||"
+    _commands = [
         (
             "-CreateDBConnection",
-            ("--Server", "--Database", "--Connection", "--Username", "--Password"),
-            (True, True, False, False, False),
-            "{{AUTOMATE}} /setdbserver {{--Server}} /setdbname {{--Database}} /dbconname {{--Connection}} //setdbusername {{--Username}} /setdbpassword {{--Password}}"
+            [
+                ("--InstallDirectory", True),
+                ("--Server", True),
+                ("--Database", True),
+                ("--Connection", False),
+                ("--Username", False),
+                ("--Password", False)
+
+            ],
+            [
+                "{{--InstallDirectory}}\\Automate.exe",
+                "/setdbserver {{--Server}}",
+                "/setdbname {{--Database}}",
+                "/dbconname {{--Connection}}",
+                "/setdbusername {{--Username}}",
+                "/setdbpassword {{--Password}}"
+            ]
         ),
         (
             "-CreateBPConnection",
-            ("--Host", "--Port", "--Connection", "--Mode"),
-            (True, True, False, False),
-            "{{AUTOMATE}} /setbpserver {{-HOST}} {{--PORT}} /dbconname {{--Connection}} /connectionmode {{--Mode}}"
+            [
+                ("--InstallDirectory", True),
+                ("--Host", True),
+                ("--Port", True),
+                ("--Connection", False),
+                ("--Mode", False)
+            ],
+            [
+                "{{--InstallDirectory}}\\Automate.exe",
+                "/setbpserver {{--Host}} {{--Port}}",
+                "/dbconname {{--Connection}}",
+                "/connectionmode {{--Mode}}"
+            ]
         ),
         (
             "-LaunchBP",
-            (),
-            (),
-            "{{AUTOMATE}}"
+            [
+                ("--InstallDirectory", True)
+            ],
+            [
+                "{{--InstallDirectory}}\\Automate.exe"
+            ]
         ),
         (
             "-CreateDB",
-            ("--Connection"),
-            (False),
-            "{{AUTOMATE_C}} /createdb"
+            [
+                ("--InstallDirectory", True),
+                ("--Connection", False)
+            ],
+            [
+                "{{--InstallDirectory}}\\AutomateC.exe",
+                "/createdb",
+                "/dbconname {{--Connection}}"
+            ]
+
         )
     ]
 
     def __init__(self, install_dir=""):
 
-        id = self._directory + self._standard_bp_install_folder
-
         if install_dir != "":
-            id = install_dir
+            self._install_directory = install_dir
 
-        self._automate = id + self._automate_exe
-        self._automate_c = id + self._automate_c_exe
+    def command(self, command, arguments):
 
+        comm = self._get_command(command)
+        args = self._add_install_directory_key(arguments)
+        arr = self._replace_arguments_in_command(comm[2], args)
+        if self._missing_mandatory_arguments(arr, comm[1]):
+            raise Exception("Missing mandatory argument.")
+        return self._remove_placeholder_arguments(arr)
+
+    def _get_command(self, comm):
+
+        for c in self._commands:
+            if c[0] == comm:
+                return c
+
+        raise Exception("Unable to locate command.")
+
+    def _add_install_directory_key(self, args):
+
+        for a in args:
+            if a[0] == "--InstallDirectory":
+                return args
+
+        return [("--InstallDirectory", self._install_directory)] + args
+
+    def _replace_arguments_in_command(self, arr, args):
+
+        string = self._delimiter.join(arr)
+
+        for a in args:
+            string = string.replace("{{"+a[0]+"}}", a[1])
+
+        return string.split(self._delimiter)
+
+    @staticmethod
+    def _missing_mandatory_arguments(arr, args):
+
+        mandatory = []
+
+        for a in args:
+            if a[1]:
+                mandatory += [a[0]]
+
+        for m in mandatory:
+            if m in "||".join(arr):
+                return True
+
+        return False
+
+    @staticmethod
+    def _remove_placeholder_arguments(arr):
+
+        command = []
+
+        for i in range(len(arr)):
+            if not (("{{" in arr[i]) and ("}}" in arr[i])):
+                command += [arr[i]]
+
+        return command
